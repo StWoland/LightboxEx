fe967d039e12f315651d3d67d2a4e92064d24912
Mikhail Shalai <mikhail@noinc.com>
2012-01-09 14:23:02 -0500

    baseUrl setting added

--- snippet.js
+++ snippet.js
@@ -1,15 +1,16 @@
 jQuery(document).ready(function ($) {
 	var path = $('script[src*="lightbox"]').attr('src').split("/");
-	var BASE_URL = path.slice(0, -1).join('/') + "/" ;
+	var BASE_URL = path.slice(0, -1).join('/') + "/images/" ;
 	var lightBox = $.fn.lightBox ;
 	$.fn.lightBox = function(settings) {
+		BASE_URL = ( settings && settings.baseUrl ) ? settings.baseUrl : BASE_URL ;
 		settings = $.extend({
-			imageLoading: BASE_URL + 'images/lightbox-ico-loading.gif'
-			,imageBtnPrev: BASE_URL + 'images/lightbox-btn-prev.gif'
-			,imageBtnNext: BASE_URL + 'images/lightbox-btn-next.gif'
-			,imageBtnClose: BASE_URL + 'images/lightbox-btn-close.gif'
-			,imageBlank: BASE_URL + 'images/lightbox-blank.gif'
-		}, settings);
+			imageLoading: BASE_URL + 'lightbox-ico-loading.gif'
+			,imageBtnPrev: BASE_URL + 'lightbox-btn-prev.gif'
+			,imageBtnNext: BASE_URL + 'lightbox-btn-next.gif'
+			,imageBtnClose: BASE_URL + 'lightbox-btn-close.gif'
+			,imageBlank: BASE_URL + 'lightbox-blank.gif'
+		},settings);
 		lightBox.bind(this)(settings);
 	};
 });
